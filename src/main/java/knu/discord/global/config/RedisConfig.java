f9e78e2c6cc5766067bdde9c5bb08931d7f7545e
yso8296 <yso829612@naver.com>
2025-02-13 01:50:08 +0900

    Fix: deploy.yml

--- src/main/java/knu/discord/global/config/RedisConfig.java
+++ src/main/java/knu/discord/global/config/RedisConfig.java
@@ -11,10 +11,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
+    @Value("${redis.host}")
     private String host;
 
-    @Value("${spring.data.redis.port}")
+    @Value("${redis.port}")
     private int port;
 
     @Bean
