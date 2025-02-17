5d275cf06ac6a7670162a4f1df3ab5e6afe0abab
yso8296 <yso829612@naver.com>
2025-02-17 23:58:29 +0900

    Feat: test.yml 테스트

--- src/main/java/knu/discord/global/config/RedisConfig.java
+++ src/main/java/knu/discord/global/config/RedisConfig.java
@@ -15,10 +15,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
+    @Value("${spring.data.redis.host:localhost}")
     private String host;
 
-    @Value("${spring.data.redis.port}")
+    @Value("${spring.data.redis.port:6379}")
     private int port;
 
     private static final String REDISSON_HOST_PREFIX = "redis://";
