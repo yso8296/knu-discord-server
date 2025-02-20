c4924c1045ee7336c8f2dba8be19d181cf5ee6d7
yso8296 <yso829612@naver.com>
2025-02-20 22:58:28 +0900

    Test: redis 없이 부하테스트

--- src/main/java/knu/discord/notice/service/NoticeService.java
+++ src/main/java/knu/discord/notice/service/NoticeService.java
@@ -25,14 +25,14 @@ public class NoticeService {
 
     //@Transactional
     @RedissonLock(value = "#noticeId")
-    public NoticeResponse.Link processNoticeRedirect(Long id, HttpServletRequest request) {
-        Notice notice = noticeRepository.findById(id)
+    public NoticeResponse.Link processNoticeRedirect(Long noticeId, HttpServletRequest request) {
+        Notice notice = noticeRepository.findById(noticeId)
                 .orElseThrow(() -> new EntityNotFoundException(MessageConstants.NOTICE_NOT_FOUND_MESSAGE));
 
         String ip = getClientIP(request);
         // IP 체크 키: notice:{id}:ip:{ip}
-        String ipKey = "notice:" + id + ":ip:" + ip;
-        String viewKey = "notice:" + id + ":views";
+        String ipKey = "notice:" + noticeId + ":ip:" + ip;
+        String viewKey = "notice:" + noticeId + ":views";
 
         // IP 키가 존재하지 않으면 조회수 증가
         /*Boolean exists = redisTemplate.hasKey(ipKey);
@@ -40,7 +40,7 @@ public class NoticeService {
             redisTemplate.opsForValue().increment(viewKey, 1);
             redisTemplate.opsForValue().set(ipKey, "1", 24, TimeUnit.HOURS);
         }*/
-        noticeRepository.updateViewCount(id, 1);
+        noticeRepository.updateViewCount(noticeId, 1);
         String safeLink = notice.getLink().replace(">", "%3E");
 
         return NoticeResponse.Link.from(safeLink);
